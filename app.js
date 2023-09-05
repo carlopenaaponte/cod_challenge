73308baa56b88c64432c2bfcb7d765d7ef253df6
carlopenaaponte <programadorcarlo@hotmail.com>
2023-09-04 22:50:22 -0300

    challenge

---
+++ app.js
@@ -0,0 +1,32 @@
+document.getElementById("registrar").addEventListener('submit',function(e){
+    e.preventDefault();
+
+URL = 'https://jsonplaceholder.typicode.com/users';
+
+const nombre = document.getElementById("nombre").value;
+const apellido = document.getElementById("apellido").value;
+const fecha = document.getElementById("fecha").value;
+
+const data = {
+    Nombre: nombre,
+    Apellido: apellido,
+    Fecha: fecha
+};
+
+fetch(URL, {
+ method : 'POST',
+ headers : {
+    'Content-Type': 'application/json'
+ },
+ body: JSON.stringify(data)    
+})
+
+.then(response => response.json())
+.then(data => {
+    document.getElementById('mostrarInfo').innerHTML = JSON.stringify(data)
+
+})
+.catch(error => {
+    console.error('error:', error);
+    })
+})
